02c3a25fee106ff5cb5d51e29dceced0aeeb2379
Blergborg <pprater92@gmail.com>
2023-09-02 17:21:49 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Phil Prater on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
