18bd189c61cf0427356970b0d8235c977e9da70d
Blergborg <pprater92@gmail.com>
2023-09-04 14:28:18 -0700

    some animation basics. make a button do different things when pressed with common built-in animations and setting up more custom animations with the .animation() modifier.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Tap Me") {
+//                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            // custom animation for a pulse off the button
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+            )
+            .onAppear {
+                // makes the pulse animation start once button view is available
+                animationAmount = 2
+            }
+            /*
+             // custom animation modifier that makes button grow and shrink 3x when pressed
+            .animation(
+                .easeInOut(duration: 1)
+//                    .delay(1)
+//                    .repeatCount(3, autoreverses: true),
+                    .repeatForever(autoreverses: false),
+                value: animationAmount
+            )
+            // custom animation that makes button grow and shrink in a spring-y motion.
+//            .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+             */
         }
         .padding()
     }
