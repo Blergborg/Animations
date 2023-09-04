0a7339a2c521234aca127ce3db6b2249ae2e3c11
Blergborg <pprater92@gmail.com>
2023-09-04 16:43:41 -0700

    Reset button to more basic version. Added Stepper that binds to animationAmount state and has a .animation() modifier attached to it. Changing state value with stepper cause the animation to occur, while directly clicking the button does not trigger animation.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,44 +11,24 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        VStack {
+        print(animationAmount)
+        return VStack {
+            // Tapping triggers animation b/c we attached a .animation() modifier to the binding variable
+            Stepper("Scale amount", value:
+                    $animationAmount.animation(), in: 1...10)
+
+            Spacer()
+
+            // Tapping button changes size immediately
             Button("Tap Me") {
-//                animationAmount += 1
+                animationAmount += 1
             }
-            .padding(50)
+            .padding(40)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            // custom animation for a pulse off the button
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeInOut(duration: 1)
-                            .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-            )
-            .onAppear {
-                // makes the pulse animation start once button view is available
-                animationAmount = 2
-            }
-            /*
-             // custom animation modifier that makes button grow and shrink 3x when pressed
-            .animation(
-                .easeInOut(duration: 1)
-//                    .delay(1)
-//                    .repeatCount(3, autoreverses: true),
-                    .repeatForever(autoreverses: false),
-                value: animationAmount
-            )
-            // custom animation that makes button grow and shrink in a spring-y motion.
-//            .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
-             */
+            .scaleEffect(animationAmount)
         }
-        .padding()
     }
 }
 
