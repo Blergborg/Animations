1bfddc9e94e68d18a21e6038ed043836d529e810
Blergborg <pprater92@gmail.com>
2023-09-04 19:14:11 -0700

    Added example of a view that has a transition for when its visibility is toggled.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,51 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
     
     var body: some View {
-        // can apply animation to each individual letter to make a Slinky-like effect when dragging the word container around.
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+//                    .transition(.scale) // NOTE: insert animation work when actually running the simulator, preview is broken for some reason?
+                // .asymmetric modifier allows us to use different transitions for view entrance vs exit.
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
-        /*
-        // Basic Card with drag gesture that springs back to original spot when drag ends.
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            // offset modifier allow a View's position to change, w/o affecting other Views.
-            .offset(dragAmount)
-            // adding gesture support for a View
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in  // remember, this says ignore args
-                        // explicit animation
-                        withAnimation(.spring()) {
-                            dragAmount = .zero
-                        }
-                    }
-            )
-        // applied animation
-//            .animation(.spring(), value: dragAmount)
-       */
     }
 }
 
