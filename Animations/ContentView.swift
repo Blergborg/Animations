c5bc2b60ce133a35fa631601090f09f3416ffb64
Blergborg <pprater92@gmail.com>
2023-09-04 20:39:19 -0700

    Added example of how to implement a custom transition. (Mimics the Pivot animation in Keynote – it causes a new slide to rotate in from its top-left corner)

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,22 +11,45 @@ struct ContentView: View {
     @State private var isShowingRed = false
     
     var body: some View {
-        VStack {
-            Button("Tap Me") {
-                withAnimation {
-                    isShowingRed.toggle()
-                }
-            }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
 
             if isShowingRed {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-//                    .transition(.scale) // NOTE: insert animation work when actually running the simulator, preview is broken for some reason?
-                // .asymmetric modifier allows us to use different transitions for view entrance vs exit.
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)  // can now use our new "pivot" transiton
             }
         }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
+
+// creating a custom modifier for the transition
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint  // allows us to set the X/Y point of the anchor.
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)  // spins on Z axis.
+            .clipped()  // prevents view from being drawn outside of its rectangular space.
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        // modifier transition allows us to 
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
     }
 }
 
